5a487f732b3ea3d239c94ba8fe5a0a9501b8bbac
lxxxxx-oss <2238428563@qq.com>
2022-12-06 10:27:52 +0800

    根据用户位置信息进行种植建议

--- src/views/Proposal.vue
+++ src/views/Proposal.vue
@@ -1,10 +1,27 @@
 <template>
   <div class="location">
-    <el-button type="text" @click="open">点击获取当前您的位置信息</el-button>
+    <el-button type="text" @click="open" class="infoBtn">点击获取当前您的位置信息</el-button>
+    <el-divider></el-divider>
     <!-- 经度 -->
-    <div>经度:{{lng}}</div>
+    <div>经度:<i style="color: rgb(106, 255, 0);margin-left: 10px;font-size: 30px;">{{lng}}</i></div>
     <!-- 纬度 -->
-    <div>纬度:{{lat}}</div>
+    <div>纬度:<i style="color: rgb(106, 255, 0);margin-left: 10px;font-size: 30px;">{{lat}}</i></div>
+    <el-divider></el-divider>
+    <!-- 后端返回的数据，建立的种植意见 -->
+    <div class="proposal">
+      <div>种植建议</div>
+      <!-- 接收到了信息则对信息进行展示 -->
+      <div class="plantInfo" v-if="plantProposal">
+        <div>您所在地区的地理环境特点</div>
+        <div>适合种植的植物</div>
+        <el-divider></el-divider>
+      </div>
+      <!-- 还未接收到后端信息时的提示信息 -->
+      <div class="plantInfo" else>
+        <div>您还未上传您的位置信息哦！</div>
+        <el-divider></el-divider>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,7 +32,9 @@
       return{
         // 经纬度
         lng: '',
-        lat: ''
+        lat: '',
+        // 后端返回的信息
+        plantProposal: ''
       }
     },
     methods: {
@@ -23,22 +42,21 @@
           this.$confirm('是否允许获取您当前的位置信息', '提示', {
             confirmButtonText: '确定',
             cancelButtonText: '取消',
-            type: 'warning'
+            type: 'info'
           }).then(() => {
             this.getLocation();
             this.$message({
               type: 'success',
-              message: '获取成功!'
+              message: '获取成功'
             });
           }).catch(() => {
             this.$message({
-              type: 'info',
+              type: 'warning',
               message: '获取失败'
             });          
           });
         },
         getLocation() {
-          //Toast("如长时间未获取办理区域请手动选择");
           myBMap.init().then(() => {
             let that = this;
             let geolocation = new BMap.Geolocation(); 
@@ -60,6 +78,7 @@
               }
             });
           });
+          // 调用方法，将经纬度信息传递给后端，同时接收后端返回的建议信息
         },
     }
   }
@@ -68,11 +87,26 @@
 
 <style>
   .location {
-    height: 100%;
-    width: 100%;
     margin: 10px;
-    padding: 10px;
-    font-size: 35px;
+    padding: 20px;
+    font-size: 25px;
+    /* background-color:  rgb(106, 255, 0); */
     box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)
+  }
+  .infoBtn {
+    font-size: 30px;
+    font-weight: 550;
+    padding-bottom: 5px;
+    border-bottom: 2px solid #cecece;
+  }
+  .proposal {
+    margin: 20px 0 0 0;
+    font-weight: 600;
+  }
+  .plantInfo {
+    margin: 20px 0 0 0;
+    font-size: 20px;
+    font-weight: 500;
+    color: rgb(245, 76, 76);
   }
 </style>
